ff8eb3245acf4e9036e7ed27358592a62145a055
Elena Guzman <elena.guzman@bravent.net>
2019-09-27 13:41:37 +0200

    Add warrior image to tables and forms

--- BlazorApp1.DataAccess/Migrations/20190927110724_imageurl-field.Designer.cs
+++ BlazorApp1.DataAccess/Migrations/20190927110724_imageurl-field.Designer.cs
@@ -4,14 +4,16 @@ using BlazorApp1.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlazorApp1.DataAccess.Migrations
 {
     [DbContext(typeof(DemoContext))]
-    partial class DemoContextModelSnapshot : ModelSnapshot
+    [Migration("20190927110724_imageurl-field")]
+    partial class imageurlfield
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
